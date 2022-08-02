869c5efa7a9eee61f42417a9ffc19acae7d8b8a2
Jude Zhu <jude.zhu@dapperlabs.com>
2022-08-02 16:36:32 -0700

    clean up

--- edition-nft/lib/go/test/templates.go
+++ edition-nft/lib/go/test/templates.go
@@ -10,44 +10,28 @@ import (
 
 const (
 	nftAddressPlaceholder            = "\"[^\"]*NonFungibleToken.cdc\""
-	AllDayAddressPlaceholder         = "\"[^\"]*AllDay.cdc\""
 	AllDaySeasonalAddressPlaceholder = "\"[^\"]*AllDaySeasonal.cdc\""
 )
 
 const (
-	AllDayPath                 = "../../../contracts/AllDay.cdc"
-	AllDaySeasonalPath         = "../../../contracts/AllDaySeasonal.cdc"
-	AllDayTransactionsRootPath = "../../../transactions"
-	AllDayScriptsRootPath      = "../../../scripts"
+	AllDaySeasonalPath   = "../../../contracts/AllDaySeasonal.cdc"
+	TransactionsRootPath = "../../../transactions"
+	ScriptsRootPath      = "../../../scripts"
 
 	// Accounts
-	AllDaySetupAccountPath         = AllDayTransactionsRootPath + "/user/setup_allday_account.cdc"
-	AllDaySeasonalSetupAccountPath = AllDayTransactionsRootPath + "/user/setup_allday_seasonal_account.cdc"
-	IsAccountSetupScriptPath       = AllDayScriptsRootPath + "/user/is_account_setup.cdc"
+	SetupAccountTxPath       = TransactionsRootPath + "/user/setup_allday_seasonal_account.cdc"
+	IsAccountSetupScriptPath = ScriptsRootPath + "/user/is_account_setup.cdc"
 
 	// Editions
-	AllDayCreateEditionPath         = AllDayTransactionsRootPath + "/admin/editions/create_edition.cdc"
-	AllDaySeasonalCreateEditionPath = AllDayTransactionsRootPath + "/admin/editions/create_seasonal_edition.cdc"
-	AllDayCloseEditionPath          = AllDayTransactionsRootPath + "/admin/editions/close_edition.cdc"
-	AllDaySeasonalCloseEditionPath  = AllDayTransactionsRootPath + "/admin/editions/close_seasonal_edition.cdc"
+	CreateEditionTxPath       = TransactionsRootPath + "/admin/editions/create_seasonal_edition.cdc"
+	CloseEditionTxPath        = TransactionsRootPath + "/admin/editions/close_seasonal_edition.cdc"
+	ReadEditionByIDScriptPath = ScriptsRootPath + "/editions/read_seasonal_edition_by_id.cdc"
 
-	AllDayReadEditionByIDPath         = AllDayScriptsRootPath + "/editions/read_edition_by_id.cdc"
-	AllDaySeasonalReadEditionByIDPath = AllDayScriptsRootPath + "/editions/read_seasonal_edition_by_id.cdc"
-	AllDayReadAllEditionsPath         = AllDayScriptsRootPath + "/edition/read_all_editions.cdc"
-
-	// Moment NFTs
-	AllDayMintMomentNFTPath   = AllDayTransactionsRootPath + "/admin/nfts/mint_moment_nft.cdc"
-	AllDayMintSeasonalNFTPath = AllDayTransactionsRootPath + "/admin/nfts/mint_seasonal_nft.cdc"
-
-	AllDayMintMomentNFTMultiPath        = AllDayTransactionsRootPath + "/admin/nfts/mint_moment_nft_multi.cdc"
-	AllDayTransferNFTPath               = AllDayTransactionsRootPath + "/user/transfer_moment_nft.cdc"
-	AllDayReadMomentNFTSupplyPath       = AllDayScriptsRootPath + "/nfts/read_moment_nft_supply.cdc"
-	AllDayReadSeasonalNFTSupplyPath     = AllDayScriptsRootPath + "/nfts/read_seasonal_nft_supply.cdc"
-	AllDayReadMomentNFTPropertiesPath   = AllDayScriptsRootPath + "/nfts/read_moment_nft_properties.cdc"
-	AllDayReadSeasonalNFTPropertiesPath = AllDayScriptsRootPath + "/nfts/read_seasonal_nft_properties.cdc"
-
-	AllDayReadCollectionNFTLengthPath = AllDayScriptsRootPath + "/nfts/read_collection_nft_length.cdc"
-	AllDayReadCollectionNFTIDsPath    = AllDayScriptsRootPath + "/nfts/read_collection_nft_ids.cdc"
+	// NFTs
+	MintNFTTxPath           = TransactionsRootPath + "/admin/nfts/mint_seasonal_nft.cdc"
+	AllDayTransferNFTPath   = TransactionsRootPath + "/user/transfer_moment_nft.cdc"
+	ReadNftSupplyScriptPath = ScriptsRootPath + "/nfts/read_seasonal_nft_supply.cdc"
+	ReadNftPropertiesTxPath = ScriptsRootPath + "/nfts/read_seasonal_nft_properties.cdc"
 )
 
 //------------------------------------------------------------
@@ -56,9 +40,6 @@ const (
 func replaceAddresses(code []byte, contracts Contracts) []byte {
 	nftRe := regexp.MustCompile(nftAddressPlaceholder)
 	code = nftRe.ReplaceAll(code, []byte("0x"+contracts.NFTAddress.String()))
-
-	AllDayRe := regexp.MustCompile(AllDayAddressPlaceholder)
-	code = AllDayRe.ReplaceAll(code, []byte("0x"+contracts.AllDayAddress.String()))
 
 	AllDaySeasonalRe := regexp.MustCompile(AllDaySeasonalAddressPlaceholder)
 	code = AllDaySeasonalRe.ReplaceAll(code, []byte("0x"+contracts.AllDayAddress.String()))
@@ -77,7 +58,7 @@ func allDaySeasonalContract(nftAddress flow.Address) []byte {
 
 func setupAccountTransaction(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDaySeasonalSetupAccountPath),
+		readFile(SetupAccountTxPath),
 		contracts,
 	)
 }
@@ -91,69 +72,48 @@ func isAccountSetupScript(contracts Contracts) []byte {
 
 func createEditionTransaction(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDaySeasonalCreateEditionPath),
+		readFile(CreateEditionTxPath),
 		contracts,
 	)
 }
 
 func readEditionByIDScript(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDaySeasonalReadEditionByIDPath),
+		readFile(ReadEditionByIDScriptPath),
 		contracts,
 	)
 }
 
 func closeEditionTransaction(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDaySeasonalCloseEditionPath),
+		readFile(CloseEditionTxPath),
 		contracts,
 	)
 }
-
-//func loadAllDayReadAllEditionsScript(contracts Contracts) []byte {
-//	return replaceAddresses(
-//		readFile(AllDayReadAllEditionsPath),
-//		contracts,
-//	)
-//}
 
 //------------------------------------------------------------
 // Moment NFTs
 //------------------------------------------------------------
 func mintEditionNFTTransaction(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDayMintSeasonalNFTPath),
+		readFile(MintNFTTxPath),
 		contracts,
 	)
 }
 
 func getEditionNFTSupplyScript(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDayReadSeasonalNFTSupplyPath),
+		readFile(ReadNftSupplyScriptPath),
 		contracts,
 	)
 }
 
 func getEditionNFTPropertiesScript(contracts Contracts) []byte {
 	return replaceAddresses(
-		readFile(AllDayReadSeasonalNFTPropertiesPath),
+		readFile(ReadNftPropertiesTxPath),
 		contracts,
 	)
 }
-
-//func readCollectionNFTLengthScript(contracts Contracts) []byte {
-//	return replaceAddresses(
-//		readFile(AllDayReadCollectionNFTLengthPath),
-//		contracts,
-//	)
-//}
-//
-//func loadAllDayReadCollectionNFTIDsScript(contracts Contracts) []byte {
-//	return replaceAddresses(
-//		readFile(AllDayReadCollectionNFTIDsPath),
-//		contracts,
-//	)
-//}
 
 func loadAllDayTransferNFTTransaction(contracts Contracts) []byte {
 	return replaceAddresses(
