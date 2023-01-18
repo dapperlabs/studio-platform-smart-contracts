755463aed55e746542283a078972235b8b592a12
Jude Zhu <jude.zhu@dapperlabs.com>
2023-01-18 09:48:59 -0800

    remove unused tests

--- pds/lib/go/test/pds_test.go
+++ pds/lib/go/test/pds_test.go
@@ -4,27 +4,6 @@ import (
 	"testing"
 )
 
-//func TestPDSDeployment(t *testing.T) {
-//	b, accountKeys := newTestSetup(t)
-//
-//	// Create new keys for the NFT contract account
-//	// and deploy all the NFT contracts
-//	exampleNFTAccountKey, _ := accountKeys.NewWithSigner()
-//	nftAddress, _, exampleNFTAddress, _, _ := deployPDSContracts(t, b, exampleNFTAccountKey, exampleNFTAccountKey)
-//
-//	t.Run("Should have properly initialized fields after deployment", func(t *testing.T) {
-//
-//		//script := templates.GenerateGetTotalSupplyScript(nftAddress, exampleNFTAddress)
-//		//supply := executeScriptAndCheck(t, b, script, nil)
-//		//assert.Equal(t, cadence.NewUInt64(0), supply)
-//		//
-//		//assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
-//		//	exampleNFTAddress,
-//		//	0,
-//		//)
-//	})
-//}
-
 // Create all required resources for different accounts
 func TestMintExampleNFTs(t *testing.T) {
 	b, accountKeys := newTestSetup(t)
@@ -58,23 +37,5 @@ func TestMintExampleNFTs(t *testing.T) {
 		//	exampleNFTAddress,
 		//	1,
 		//)
-	})
-}
-
-// Create all required resources for different accounts
-func TestMintPackNFTs(t *testing.T) {
-	b, accountKeys := newTestSetup(t)
-
-	exampleNFTAccountKey, exampleNFTSigner := accountKeys.NewWithSigner()
-	nftAddress, metadataAddress, exampleNFTAddress, _, _ := deployPDSContracts(t, b, exampleNFTAccountKey, exampleNFTAccountKey)
-
-	t.Run("Should be able to mint a token", func(t *testing.T) {
-		// Mint a single NFT with standard royalty cuts and metadata
-		mintExampleNFT(t, b,
-			accountKeys,
-			nftAddress, metadataAddress, exampleNFTAddress,
-			exampleNFTAccountKey,
-			exampleNFTSigner)
-
 	})
 }
