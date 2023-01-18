fbc17f1a0d38cf46b50e70e8ea57854ff84a1753
Jude Zhu <jude.zhu@dapperlabs.com>
2023-01-17 17:02:13 -0800

    added mint example nft test

--- pds/lib/go/test/pds_test.go
+++ pds/lib/go/test/pds_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"github.com/dapperlabs/studio-platform-smart-contracts/lib/go/templates"
 	"github.com/onflow/cadence"
+	jsoncdc "github.com/onflow/cadence/encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -21,9 +22,45 @@ func TestPDSDeployment(t *testing.T) {
 		supply := executeScriptAndCheck(t, b, script, nil)
 		assert.Equal(t, cadence.NewUInt64(0), supply)
 
-		//assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
-		//	exampleNFTAddress,
-		//	0,
-		//)
+		assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
+			exampleNFTAddress,
+			0,
+		)
+	})
+}
+
+// Create all required resources for different accounts
+func TestMintExampleNFTs(t *testing.T) {
+	b, accountKeys := newTestSetup(t)
+
+	exampleNFTAccountKey, exampleNFTSigner := accountKeys.NewWithSigner()
+	nftAddress, metadataAddress, exampleNFTAddress, _, _ := deployPDSContracts(t, b, exampleNFTAccountKey, exampleNFTAccountKey)
+
+	t.Run("Should be able to mint a token", func(t *testing.T) {
+		// Mint a single NFT with standard royalty cuts and metadata
+		mintExampleNFT(t, b,
+			accountKeys,
+			nftAddress, metadataAddress, exampleNFTAddress,
+			exampleNFTAccountKey,
+			exampleNFTSigner)
+
+		script := templates.GenerateBorrowNFTScript(nftAddress, exampleNFTAddress)
+		executeScriptAndCheck(
+			t, b,
+			script,
+			[][]byte{
+				jsoncdc.MustEncode(cadence.NewAddress(exampleNFTAddress)),
+				jsoncdc.MustEncode(cadence.NewUInt64(0)),
+			},
+		)
+
+		script = templates.GenerateGetTotalSupplyScript(nftAddress, exampleNFTAddress)
+		supply := executeScriptAndCheck(t, b, script, nil)
+		assert.Equal(t, cadence.NewUInt64(1), supply)
+
+		assertCollectionLength(t, b, nftAddress, exampleNFTAddress,
+			exampleNFTAddress,
+			1,
+		)
 	})
 }
--- pds/lib/go/test/pds_test_helper.go
+++ pds/lib/go/test/pds_test_helper.go
@@ -3,7 +3,10 @@ package test
 import (
 	"encoding/hex"
 	studioPlatformContracts "github.com/dapperlabs/studio-platform-smart-contracts/lib/go/contracts"
+	"github.com/onflow/flow-nft/lib/go/templates"
+
 	"github.com/onflow/cadence"
+	jsoncdc "github.com/onflow/cadence/encoding/json"
 	emulator "github.com/onflow/flow-emulator"
 	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/crypto"
@@ -128,4 +131,115 @@ func deployPDSContract(t *testing.T, b *emulator.Blockchain, nftAddress, iPackNF
 	require.NoError(t, err)
 
 	return PDSAddress
+}
+
+// Assers that the ExampleNFT collection in the specified user's account
+// is the expected length
+func assertCollectionLength(
+	t *testing.T,
+	b *emulator.Blockchain,
+	nftAddress flow.Address, exampleNFTAddress flow.Address,
+	collectionAddress flow.Address,
+	expectedLength int,
+) {
+	script := templates.GenerateGetCollectionLengthScript(nftAddress, exampleNFTAddress)
+	actualLength := executeScriptAndCheck(t, b, script, [][]byte{jsoncdc.MustEncode(cadence.NewAddress(collectionAddress))})
+	assert.Equal(t, cadence.NewInt(expectedLength), actualLength)
+}
+
+// Mints a single NFT from the ExampleNFT contract
+// with standard metadata fields and royalty cuts
+func mintExampleNFT(
+	t *testing.T,
+	b *emulator.Blockchain,
+	accountKeys *test.AccountKeys,
+	nftAddress, metadataAddress, exampleNFTAddress flow.Address,
+	exampleNFTAccountKey *flow.AccountKey,
+	exampleNFTSigner crypto.Signer,
+) {
+
+	// Create two new accounts to act as beneficiaries for royalties
+	beneficiaryAddress1, _, beneficiarySigner1 := newAccountWithAddress(b, accountKeys)
+	setupRoyaltyReceiver(t, b,
+		metadataAddress,
+		beneficiaryAddress1,
+		beneficiarySigner1,
+	)
+	beneficiaryAddress2, _, beneficiarySigner2 := newAccountWithAddress(b, accountKeys)
+	setupRoyaltyReceiver(t, b,
+		metadataAddress,
+		beneficiaryAddress2,
+		beneficiarySigner2,
+	)
+
+	// Generate the script that mints a new NFT and deposits it into the recipient's account
+	// whose address is the first argument to the transaction
+	script := templates.GenerateMintNFTScript(nftAddress, exampleNFTAddress, metadataAddress, flow.HexToAddress(emulatorFTAddress))
+
+	// Create the transaction object with the generated script and authorizer
+	tx := createTxWithTemplateAndAuthorizer(b, script, exampleNFTAddress)
+
+	// Assemble the cut information for royalties
+	cut1 := CadenceUFix64("0.25")
+	cut2 := CadenceUFix64("0.40")
+	cuts := []cadence.Value{cut1, cut2}
+
+	// Assemble the royalty description and beneficiary addresses to get their receivers
+	royaltyDescriptions := []cadence.Value{cadence.String("Minter royalty"), cadence.String("Creator royalty")}
+	royaltyBeneficiaries := []cadence.Value{cadence.NewAddress(beneficiaryAddress1), cadence.NewAddress(beneficiaryAddress2)}
+
+	// First argument is the recipient of the newly minted NFT
+	tx.AddArgument(cadence.NewAddress(exampleNFTAddress))
+	tx.AddArgument(cadence.String("Example NFT 0"))
+	tx.AddArgument(cadence.String("This is an example NFT"))
+	tx.AddArgument(cadence.String("example.jpeg"))
+	tx.AddArgument(cadence.NewArray(cuts))
+	tx.AddArgument(cadence.NewArray(royaltyDescriptions))
+	tx.AddArgument(cadence.NewArray(royaltyBeneficiaries))
+
+	serviceSigner, _ := b.ServiceKey().Signer()
+
+	signAndSubmit(
+		t, b, tx,
+		[]flow.Address{
+			b.ServiceKey().Address,
+			exampleNFTAddress,
+		},
+		[]crypto.Signer{
+			serviceSigner,
+			exampleNFTSigner,
+		},
+		false,
+	)
+}
+
+// Sets up an account with the generic royalty receiver in place of their Flow token receiver
+func setupRoyaltyReceiver(
+	t *testing.T,
+	b *emulator.Blockchain,
+	metadataAddress flow.Address,
+	authorizerAddress flow.Address,
+	authorizerSigner crypto.Signer,
+) {
+
+	script := templates.GenerateSetupAccountToReceiveRoyaltyScript(metadataAddress, flow.HexToAddress(emulatorFTAddress))
+	tx := createTxWithTemplateAndAuthorizer(b, script, authorizerAddress)
+
+	vaultPath := cadence.Path{Domain: "storage", Identifier: "flowTokenVault"}
+	tx.AddArgument(vaultPath)
+
+	serviceSigner, _ := b.ServiceKey().Signer()
+
+	signAndSubmit(
+		t, b, tx,
+		[]flow.Address{
+			b.ServiceKey().Address,
+			authorizerAddress,
+		},
+		[]crypto.Signer{
+			serviceSigner,
+			authorizerSigner,
+		},
+		false,
+	)
 }
