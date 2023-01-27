c2672eac0c641afb096ee53d379fffc5521eca1a
Jude Zhu <jude.zhu@dapperlabs.com>
2023-01-27 15:58:40 -0800

    remove fungible token

--- pds/lib/go/test/pds_test_helper.go
+++ pds/lib/go/test/pds_test_helper.go
@@ -34,7 +34,7 @@ func deployPDSContracts(
 
 	// 1. Deploy utility contracts
 	nftAddress := deploy(t, b, "NonFungibleToken", contracts.NonFungibleToken())
-	ftAddress := deploy(t, b, "FungibleToken", studioPlatformContracts.FungibleToken())
+	//ftAddress := deploy(t, b, "FungibleToken", studioPlatformContracts.FungibleToken())
 
 	metadataAddress := deploy(t, b, "MetadataViews", contracts.MetadataViews(flow.HexToAddress(emulatorFTAddress), nftAddress))
 	exampleNFTAddress := deploy(
