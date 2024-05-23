1b1309b8eda7d753d6b1d4e6ede07769ba727bc5
Jude Zhu <judezhu.hust@gmail.com>
2024-05-22 17:09:46 -0700

    added replacer

---
+++ common/utils.go
@@ -0,0 +1,36 @@
+package common
+
+import "regexp"
+
+var (
+	placeholderNonFungibleToken = regexp.MustCompile(`"[^"\s].*/NonFungibleToken.cdc"`)
+	placeholderExampleNFT       = regexp.MustCompile(`"[^"\s].*/ExampleNFT.cdc"`)
+	placeholderMetadataViews    = regexp.MustCompile(`"[^"\s].*/MetadataViews.cdc"`)
+	placeholderFungibleToken    = regexp.MustCompile(`"[^"\s].*/FungibleToken.cdc"`)
+)
+
+type Replacer map[AddressName]*regexp.Regexp
+
+type AddressName string
+
+const (
+	NonFungibleTokenAddr AddressName = "NonFungibleToken"
+)
+
+var replacer Replacer
+
+func init() {
+	replacer = map[AddressName]*regexp.Regexp{
+		"placeholderNonFungibleToken": placeholderNonFungibleToken,
+		"placeholderExampleNFT":       placeholderExampleNFT,
+		"placeholderMetadataViews":    placeholderMetadataViews,
+		"placeholderFungibleToken":    placeholderFungibleToken,
+	}
+}
+
+func (r Replacer) replaceAddresses(code string, addr map[AddressName]string) string {
+	for key, value := range addr {
+		code = r[key].ReplaceAllString(code, "0x"+value)
+	}
+	return code
+}
