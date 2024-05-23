cd4df8ea44000657a417b9413f9399e145818919
Jude Zhu <judezhu.hust@gmail.com>
2024-05-23 09:53:20 -0700

    added utils

--- common/utils.go
+++ common/utils.go
@@ -1,12 +1,16 @@
 package common
 
-import "regexp"
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+)
 
 var (
-	placeholderNonFungibleToken = regexp.MustCompile(`"[^"\s].*/NonFungibleToken.cdc"`)
-	placeholderExampleNFT       = regexp.MustCompile(`"[^"\s].*/ExampleNFT.cdc"`)
-	placeholderMetadataViews    = regexp.MustCompile(`"[^"\s].*/MetadataViews.cdc"`)
-	placeholderFungibleToken    = regexp.MustCompile(`"[^"\s].*/FungibleToken.cdc"`)
+	placeholderNonFungibleToken = regexp.MustCompile(`"[^"\s]?.*NonFungibleToken(\.cdc)?"`)
+	placeholderExampleNFT       = regexp.MustCompile(`"[^"\s].*ExampleNFT.cdc"`)
+	placeholderMetadataViews    = regexp.MustCompile(`"[^"\s].*MetadataViews.cdc"`)
+	placeholderFungibleToken    = regexp.MustCompile(`"[^"\s].*FungibleToken.cdc"`)
 )
 
 type Replacer map[AddressName]*regexp.Regexp
@@ -17,20 +21,33 @@ const (
 	NonFungibleTokenAddr AddressName = "NonFungibleToken"
 )
 
-var NewReplacer Replacer
-
-func init() {
-	NewReplacer = map[AddressName]*regexp.Regexp{
-		"placeholderNonFungibleToken": placeholderNonFungibleToken,
-		"placeholderExampleNFT":       placeholderExampleNFT,
-		"placeholderMetadataViews":    placeholderMetadataViews,
-		"placeholderFungibleToken":    placeholderFungibleToken,
+func NewReplacer() Replacer {
+	xxx := map[AddressName]*regexp.Regexp{
+		NonFungibleTokenAddr: placeholderNonFungibleToken,
 	}
+	return xxx
 }
 
-func (r Replacer) replaceAddresses(code string, addr map[AddressName]string) string {
-	for key, value := range addr {
-		code = r[key].ReplaceAllString(code, "0x"+value)
+func (r Replacer) ReplaceAddresses(code string, data interface{}) (string, error) {
+
+	val := reflect.ValueOf(data)
+	if val.Kind() != reflect.Struct {
+		return "", fmt.Errorf("data must be a struct")
 	}
-	return code
+
+	var code1 = code
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Type().Field(i)
+		fieldName := field.Name
+		fieldValue := val.Field(i).String()
+
+		placeholder := AddressName(fieldName)
+		if r[placeholder] == nil {
+			return "", fmt.Errorf("no placeholder found for %s", placeholder)
+		}
+
+		code1 = r[placeholder].ReplaceAllString(code1, "0x"+fieldValue)
+		fmt.Printf(`"%s  %s"\n`, fieldName, fieldValue)
+	}
+	return code1, nil
 }
