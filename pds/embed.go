f667a2a13aae6d82fcc38f785cf5c09dd212f980
Innocent Abdullahi <deewai48@gmail.com>
2024-08-13 09:46:46 -0700

    Update embed.go

--- pds/embed.go
+++ pds/embed.go
@@ -12,3 +12,16 @@ var Scripts embed.FS
 
 //go:embed contracts/*
 var Contracts embed.FS
+
+var (
+	//go:embed transactions/packNFT/batch_transfer_packNFTs.cdc
+	PackNFTBatchTransferPackNFT []byte
+	//go:embed transactions/packNFT/transfer_packNFT.cdc
+	PackNFTTransferPackNFT []byte
+	//go:embed transactions/pds/mint_packNFT.cdc
+	PDSMintPackNFT []byte
+	//go:embed transactions/pds/open_packNFT.cdc
+	PDSOpenPackNFT []byte
+	//go:embed transactions/pds/reveal_packNFT.cdc
+	PDSRevealPackNFT []byte
+)
