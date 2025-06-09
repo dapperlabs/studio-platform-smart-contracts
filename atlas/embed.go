8857392fe7d8edae6bc8b6d302941aaf99b8a10a
bspowell <b.sean.powell@gmail.com>
2025-06-09 12:49:35 -0700

    fix embed file

--- atlas/embed.go
+++ atlas/embed.go
@@ -6,6 +6,6 @@ import (
 
 // Transactions is a list of all the transactions we export with imports mapped
 var (
-	UserBuyPacksPrimarySale []byte
 	//go:embed transactions/user/buy_packs_primary_sale.cdc
+	UserBuyPacksPrimarySale []byte
 )
